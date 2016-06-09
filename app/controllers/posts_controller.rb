7e63f234f4e7fff3ba03106ea9adaa43f085790a
Herman Watts <hwatts@mail.umhb.edu>
2016-06-08 21:45:20 -0500

    Added Post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	def index 
+	end 
+
+	def new
+		@post = Post.new
+	end 
+
+	def create 
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Heck yeah, Herman! Your article was successfully saved!"
+		else 
+			render 'new' , notice: "Oh no, Herman! I was not able to save your post"
+		end
+	end 
+
+	def show
+	end 
+
+	def edit 
+	end 
+
+
+
+	def update 
+		if @post.update post_params
+			redirect_to @post, notcie: "Huzzah! Your article was successfully saved!"
+		else 
+			redner 'edit'
+		end 
+	end 
+
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
+private 
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end 
+ 
+end 
