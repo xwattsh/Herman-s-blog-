93cdf8be5eaecc09258d90a727e9ac46ad4cd4ee
Herman Watts <hwatts@mail.umhb.edu>
2016-06-14 18:43:59 -0500

    Set up a & installed basic project functionality

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,36 @@
+class ProjectsController < ApplicationController
+	before_action :find_project, only: [:show, :edit, :update, :destroy]
+	def index
+		@projects = Project.all.order("created_at desc")
+		#.paginate(page: params[:page], per_page: 10)
+	end 
+
+	def new 
+		@project = Project.new
+	end 
+
+	def create 
+		@project = Project.new project_params
+
+        if @project.save 
+        	redirect_to @project, notice: "Nice Herman! That project was successfully saved" 
+        else 
+        	render 'new'
+        end 
+	end 
+
+	def show 
+
+	end 
+
+private 
+
+     def find_project 
+     	@project = Project.find(params[:id])
+     end 
+    #params for the projects 
+	def project_params
+		params.require(:project).permit(:title, :description, :link)
+	end
+
+end
