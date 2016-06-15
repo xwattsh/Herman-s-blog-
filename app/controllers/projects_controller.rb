2d2b4119e76ada3944a4cae8e2ba28710d907023
Herman Watts <hwatts@mail.umhb.edu>
2016-06-14 19:16:28 -0500

    Add edit & destroy project abilities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,6 +22,20 @@ class ProjectsController < ApplicationController
 	def show 
 
 	end 
+	def edit
+	end 
+	def update 
+		if @project.update project_params
+			redirect_to @project, notice: "Nice Herman! That project was successfully updated" 
+		else
+			render 'edit'
+		end 
+	end 
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end 
 
 private 
 
